0d0009c30b2ddb02c95f18ad9163c9301d7e8658
harismkhan <97024167+harismkhan@users.noreply.github.com>
2022-01-19 09:14:33 +0100

    I changed and updated the Class Diiagramm and the Code according to the comments.

--- src/backend/ClubManagement/ClubManagement.Domain/Member.cs
+++ src/backend/ClubManagement/ClubManagement.Domain/Member.cs
@@ -11,34 +11,11 @@ namespace ClubManagement.Domain
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public DateTime BirthDate { get; set; }
-        public string Address {get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string Zip { get; set; }
         public Club Club {get; set; } 
         public Team Team { get; set; }
 
-        //public Member()
-        //{
-
-        //}
-        //public Member(
-        //    //string firstName, string lastName, DateTime birthDate, string address, Club club, Team team, float Height, float Weight, int PlayerNumber
-        //    )
-        //{
-        //    FirstName = firstName;
-        //    LastName = lastName;
-        //    BirthDate = birthDate;
-        //    Address = address;
-        //    Club = club;
-        //    team = team;
-        //}
-
-        //public Member(string firstName, string lastName, DateTime birthDate, string address, Team team, float Height, float Weight, int PlayerNumber)
-        //{
-        //    FirstName = firstName;
-        //    LastName = lastName;
-        //    BirthDate = birthDate;
-        //    Address = address;
-        //    Club = defaultClub;
-        //    Team = team;
-        //}
     }
 }
