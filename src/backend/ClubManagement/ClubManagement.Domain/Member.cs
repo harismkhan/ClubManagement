426926f384b2fdc822d7b209cf171dd69274d483
harismkhan <97024167+harismkhan@users.noreply.github.com>
2022-01-21 15:45:05 +0100

    I added the Accessibility Levels to the Class Diagram and defined a class as abstract.

--- src/backend/ClubManagement/ClubManagement.Domain/Member.cs
+++ src/backend/ClubManagement/ClubManagement.Domain/Member.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace ClubManagement.Domain
 {
-    public class Member : Base
+    public abstract class Member : Base
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
@@ -14,7 +14,7 @@ namespace ClubManagement.Domain
         public string Street { get; set; }
         public string City { get; set; }
         public string Zip { get; set; }
-        public Club Club {get; set; } 
+        public Club Club {get; set; }
         public Team Team { get; set; }
 
     }
