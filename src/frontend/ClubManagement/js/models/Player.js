9b710bfce70f8876c29bb322cc503bf4ced2b2a2
Haris Khan <haris.khan@modern-it.academy>
2022-05-20 13:35:14 +0000

    Merged PR 61: Frontend in Javascript added
    frontend implemented

---
+++ src/frontend/ClubManagement/js/models/Player.js
@@ -0,0 +1,17 @@
+class Player {
+    constructor(id, firstName, lastName, birthDate, street, city, zip, height, weight, playerNumber, clubId, teamId)
+    {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthDate = birthDate;
+        this.street = street;
+        this.city = city;
+        this.zip = zip;
+        this.height = height;
+        this.weight = weight;
+        this.playerNumber = playerNumber;
+        this.clubId = clubId;
+        this.teamId = teamId;
+    }
+}
---
+++ src/frontend/ClubManagement/js/pages/Players.js
@@ -0,0 +1,54 @@
+const playersKey = "clubmanagement.players"
+
+function onInit() {
+    let playersJson = localStorage.getItem(playersKey);
+
+    let players = PlayerParser.multipleFromJson(playersJson);
+    let tableBody = document.getElementById("playerTableBody");
+    for (var i = 0; i < players.length; i++) {
+        tableBody.innerHTML += `
+        <tr class="row" >
+            <td class="FirstName" onClick="onRowClick(${players[i].id})">${players[i].firstName}</td>
+            <td class="LastName" onClick="onRowClick(${players[i].id})">${players[i].lastName}</td>
+            <td class="BirthDate" onClick="onRowClick(${players[i].id})">${players[i].birthDate}</td>
+            <td class="Street" onClick="onRowClick(${players[i].id})">${players[i].street}</td>
+            <td class="City" onClick="onRowClick(${players[i].id})">${players[i].city}</td>
+            <td class="Zip" onClick="onRowClick(${players[i].id})">${players[i].zip}</td>
+            <td class="Height" onClick="onRowClick(${players[i].id})">${players[i].height}</td>
+            <td class="Weight" onClick="onRowClick(${players[i].id})">${players[i].weight}</td>
+            <td class="PlayerNumber" onClick="onRowClick(${players[i].id})">${players[i].playerNumber}</td>
+            <td class="deleteItem" onClick="onDelete(${players[i].id})"><i class="fa-solid fa-user-xmark"></i></i></td>
+          </a>
+        </tr>`
+            ;
+    }
+}
+function onRowClick(id) {
+    window.location.href = `./playerEdit.html?id=${id}`;
+}
+function onDelete(id) {
+    let playersJson = localStorage.getItem(playersKey) ?? "[]";
+    let players = PlayerParser.multipleFromJson(playersJson);
+    let index = players.findIndex(p => p.id == id);
+
+    if (index === -1) {
+        return;
+    }
+
+    players.splice(index, 1);
+
+    localStorage.setItem(playersKey, JSON.stringify(players));
+
+    window.location.href = "./players.html";
+
+}
+
+setTimeout(() => {
+    onInit();
+}, 1);
+
+document.addEventListener('keydown', function (event) {
+    if (event.key === "Escape") {
+        window.location.href = "../index.html";
+    }
+});
